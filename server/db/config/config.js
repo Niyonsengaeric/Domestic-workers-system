9b5ef7fb0b95175f44713d6eba13c37aa2869eef
Moise Rwibutso <eric.rwibutso@EricRwibutsos-MacBook-Pro.local>
2020-01-24 17:33:23 +0200

    [Start #170868423] feature(client-view-all-wokers-170868423): migrating the database

---
+++ server/db/config/config.js
@@ -0,0 +1,32 @@
+const  dotenv  = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_PATH,
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  }
+
+  // "test": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_test",
+  //   "host": "127.0.0.1",
+  //   "dialect": "postgres",
+  //   "operatorsAliases": false
+  // },
+  // "production": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_production",
+  //   "host": "127.0.0.1",
+  //   "dialect": "postgres",
+  //   "operatorsAliases": false
+  // }
+}
