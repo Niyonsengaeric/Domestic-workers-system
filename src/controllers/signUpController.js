bcd7a9024f165027d9070d2c83741c7ae7067190
Eric Niyonsenga <eric.niyonsenga@Erics-MacBook-Pro.local>
2020-01-28 13:55:09 +0200

    change naming

--- src/controllers/signUpController.js
+++ src/controllers/signUpController.js
@@ -1,16 +1,20 @@
-import user from '../db/models/user';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import user from '../db/models/user'
 
 export default class signUpController {
-    static async signUp (request, response){
-        const {firstname,lastname,email,password} = request.body;
-        let userIsExist = await user.findAll({ where:{email} });
-        bcrypt.hash(password,9,async(err,hashedPassword)=>{
-        if(userIsExist.length>0) return response.status(409).json({message:'User already exist'})
-       const newUser = await user.create({firstname,lastname,email,password:hashedPassword});
-        const token = jwt.sign({userId:newUser.id,email:newUser.email},'secretkey')
-        response.status(201).json({token,message:'user successfully created'})
-        })
-    }
+  static async signUp(request, response) {
+    const {
+      firstName, lastName, email, password,
+    } = request.body
+    const userIsExist = await user.findAll({ where: { email } })
+    bcrypt.hash(password, 9, async (err, hashedPassword) => {
+      if (userIsExist.length > 0) return response.status(409).json({ message: 'User already exist' })
+      const newUser = await user.create({
+        firstName, lastName, email, password: hashedPassword,
+      })
+      const token = jwt.sign({ userId: newUser.id, email: newUser.email }, 'secretkey')
+      response.status(201).json({ token, message: 'user successfully created' })
+    })
+  }
 }
