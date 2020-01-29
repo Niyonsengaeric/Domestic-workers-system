f04c823ec93afe63a052142c9443370131684693
Eric Niyonsenga <eric.niyonsenga@Erics-MacBook-Pro.local>
2020-01-29 16:48:28 +0200

    ft(test): fix testing bugs [finishes:#170973708]

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,11 +1,24 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 module.exports = {
   development: {
-    username: 'postgres',
-    password: 'qwert123',
-    database: 'domestic_workers_devdb',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_PATH,
     host: '127.0.0.1',
     dialect: 'postgres',
     operatorsAliases: false,
   },
+
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_PATH_test,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    operatorsAliases: false,
+  },
+
 }
