b75212f2f2d892bbc85970a1e5a0eab7bae4c704
Kagorora <kagororamaxime@gmail.com>
2020-01-29 19:57:03 +0200

    feature(database): add/Register new Domestic workers
    [Finishes: #170868421]

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import signUp from '../controllers/signUpController'
+import addDomesticWorker from '../controllers/addDomesticWorker'
+import domesticValidation from '../middlewares/inputValidation'
 
 const userRoutes = express.Router({ mergeParams: true })
 
 userRoutes.post('/auth/signup', signUp.signUp)
-
-
+userRoutes.post('/auth/addDomestic', domesticValidation.validateDomesticWorkers, addDomesticWorker.addDomesticWorker)
 
 export default userRoutes
