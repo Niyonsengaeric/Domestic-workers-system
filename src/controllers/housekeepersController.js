368acf212b5426923b8c360255bf8e9e72c5bc6b
Moise Rwibutso <eric.rwibutso@EricRwibutsos-MacBook-Pro.local>
2020-01-30 20:21:29 +0200

    increasing tests

--- src/controllers/housekeepersController.js
+++ src/controllers/housekeepersController.js
@@ -2,12 +2,6 @@ import models from '../db/models'
 
 class GetHouseMaids {
   static async viewAll(req, res) {
-<<<<<<< HEAD
-    const allHousekeepers = await models.user.findAll({
-      where: { type: 'maid' },
-    })
-    return res.status(200).json({ status: 200, message: 'All Housekeepers', allHousekeepers })
-=======
     try {
       const allMaids = await models.user.findAll({
         where: { type: 'maid' },
@@ -16,7 +10,6 @@ class GetHouseMaids {
     } catch (err) {
       return res.status(500).json({ Error: err })
     }
->>>>>>> increasing tests
   }
 }
 
--- src/tests/main.test.js
+++ src/tests/main.test.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import runrequestWorkerTests from './requestWorker.test'
 import testLogin from './login.spec'
 import userSignUp from './useSignup.stest';
-=======
-import userTests from './useSignup.spec'
-import runrequestWorkerTests from './requestWorker.test'
 import houseMaidsTests from './houseMaids.spec'
->>>>>>> increasing tests
 
 userSignUp()
 testLogin()
